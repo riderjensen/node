016cd78526d947f69118f4667ebbed1ebee7ac6b
Unknown <riderjensen@gmail.com>
2018-12-17 10:49:50 -0700

    Changing shop render

--- expressMod/routes/shop.js
+++ expressMod/routes/shop.js
@@ -2,11 +2,17 @@ const express = require('express');
 const path = require('path');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-	res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+	const prods = adminData.products;
+	res.render('shop', {
+		prods,
+		title: 'Shop Home',
+		path: '/'
+	})
 });
 
 module.exports = router;
