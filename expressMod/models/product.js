e0ea80a3e991969ad5beefa9d06c45dc55bb0d2f
Unknown <riderjensen@gmail.com>
2018-12-18 15:39:40 -0700

    Adding update function in save()

--- expressMod/models/product.js
+++ expressMod/models/product.js
@@ -4,7 +4,6 @@ const path = require('path');
 
 const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
 const getProductsFromFile = (cb) => {
-
 	fs.readFile(p, (err, data) => {
 		if (err) {
 			return cb([]);
@@ -15,7 +14,8 @@ const getProductsFromFile = (cb) => {
 }
 
 module.exports = class Product {
-	constructor(title, imageURL, description, price) {
+	constructor(id, title, imageURL, description, price) {
+		this.id = id;
 		this.title = title;
 		this.imageURL = imageURL;
 		this.description = description;
@@ -23,14 +23,32 @@ module.exports = class Product {
 	}
 	save() {
 		getProductsFromFile((products) => {
-			products.push(this);
-			fs.writeFile(p, JSON.stringify(products), (err) => {
-				if (err) console.log(err)
-			});
+			if (this.id) {
+				const existingProductIndex = products.findIndex(prod => prod.id === this.id);
+				const updatedProducts = [...products];
+				updatedProducts[existingProductIndex] = this;
+				fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+					if (err) console.log(err)
+				});
+			} else {
+				this.id = Math.random().toString();
+				products.push(this);
+				fs.writeFile(p, JSON.stringify(products), (err) => {
+					if (err) console.log(err)
+				});
+			}
+
 		})
 	}
 
 	static fetchAll(cb) {
 		getProductsFromFile(cb);
+	}
+
+	static findById(id, cb) {
+		getProductsFromFile(products => {
+			const product = products.find(p => p.id === id)
+			cb(product);
+		})
 	}
 }
