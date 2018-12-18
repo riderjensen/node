d48ffcce39df76c387ba373c91519b4b3d66b0a4
Unknown <riderjensen@gmail.com>
2018-12-18 15:39:09 -0700

    Changing admin controller

--- expressMod/controllers/admin.js
+++ expressMod/controllers/admin.js
@@ -1,9 +1,10 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res) => {
-	res.render('admin/add-product', {
+	res.render('admin/edit-product', {
 		title: "Add Product",
-		path: '/admin/add-product'
+		path: '/admin/add-product',
+		editing: false
 	});
 }
 
@@ -14,9 +15,39 @@ exports.postAddProduct = (req, res) => {
 		price,
 		desc
 	} = req.body;
-	const products = new Product(title, imageURL, desc, price);
+	const products = new Product(null, title, imageURL, desc, price);
 	products.save();
 	res.redirect('/');
+}
+
+exports.getEditProduct = (req, res) => {
+	const editMode = req.query.edit;
+	if (!editMode) {
+		return res.redirect('/');
+	}
+	const prodId = req.params.id;
+	Product.findById(prodId, product => {
+		if (!product) {
+			return res.redirect('/');
+		}
+		res.render('admin/edit-product', {
+			title: "Edit Product",
+			path: '/admin/add-product',
+			editing: editMode,
+			product
+		});
+	})
+}
+
+exports.postEditProduct = (req, res) => {
+	const prodId = req.body.productId;
+	const updatedTitle = req.body.title;
+	const updatedPrice = req.body.price;
+	const updatedImageURL = req.body.imageURL;
+	const updatedDescription = req.body.desc;
+	const updatedProduct = new Product(prodId, updatedTitle, updatedImageURL, updatedDescription, updatedPrice);
+	updatedProduct.save();
+	res.redirect('/admin/products');
 }
 
 exports.getProductsAll = (req, res) => {
