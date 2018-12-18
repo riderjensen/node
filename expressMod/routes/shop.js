7bed52423132ed84fc186594c473d19899ffe20c
Unknown <riderjensen@gmail.com>
2018-12-18 10:11:24 -0700

    Adding routes for new shop views

--- expressMod/routes/shop.js
+++ expressMod/routes/shop.js
@@ -1,9 +1,17 @@
 const express = require('express');
 
-const productsController = require('../controllers/products');
+const productsController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get('/', productsController.getProducts);
+router.get('/', productsController.getIndex);
+
+router.get('/products', productsController.getProducts);
+
+router.get('/cart', productsController.getCart);
+
+router.get('/checkout', productsController.getCheckout);
+
+router.get('/orders', productsController.getOrders);
 
 module.exports = router;
