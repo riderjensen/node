506c04952cb1c70543cc86aa822989710a4c4bfd
Unknown <riderjensen@gmail.com>
2018-12-19 14:25:16 -0700

    Adding in SQL queries to product

--- expressMod/models/product.js
+++ expressMod/models/product.js
@@ -1,18 +1,5 @@
-const fs = require('fs');
-const path = require('path');
-
 const Cart = require('./cart')
-
-const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-const getProductsFromFile = (cb) => {
-	fs.readFile(p, (err, data) => {
-		if (err) {
-			return cb([]);
-		}
-		cb(JSON.parse(data));
-	})
-
-}
+const db = require("../utils/database");
 
 module.exports = class Product {
 	constructor(id, title, imageURL, description, price) {
@@ -23,45 +10,18 @@ module.exports = class Product {
 		this.price = price;
 	}
 	save() {
-		getProductsFromFile((products) => {
-			if (this.id) {
-				const existingProductIndex = products.findIndex(prod => prod.id === this.id);
-				const updatedProducts = [...products];
-				updatedProducts[existingProductIndex] = this;
-				fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-					if (err) console.log(err)
-				});
-			} else {
-				this.id = Math.random().toString();
-				products.push(this);
-				fs.writeFile(p, JSON.stringify(products), (err) => {
-					if (err) console.log(err)
-				});
-			}
-
-		})
+		return db.execute('INSERT INTO products (title, price, imageURL, description) VALUES (?, ?, ?, ?)', [this.title, this.price, this.imageURL, this.description]);
 	}
 
 	static deleteById(id) {
-		getProductsFromFile(products => {
-			const product = products.find(prod => prod.id === id);
-			const updatedProducts = products.filter(prod => prod.id !== id);
-			fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-				if (!err) {
-					Cart.deleteProduct(id, product.price);
-				}
-			})
-		})
+
 	}
 
-	static fetchAll(cb) {
-		getProductsFromFile(cb);
+	static fetchAll() {
+		return db.execute('SELECT * FROM products');
 	}
 
-	static findById(id, cb) {
-		getProductsFromFile(products => {
-			const product = products.find(prod => prod.id === id)
-			cb(product);
-		})
+	static findById(id) {
+		return db.execute('SELECT * FROM products WHERE products.id = ?', [id])
 	}
 }
