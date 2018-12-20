0836ff9a200db1ade6114cffa3f5eaa39ec8a692
Unknown <riderjensen@gmail.com>
2018-12-20 10:05:52 -0700

    Working with cart model

--- expressMod/models/cart.js
+++ expressMod/models/cart.js
@@ -1,70 +1,14 @@
-const fs = require('fs');
-const path = require('path');
+const Sequelize = require('sequelize');
 
-const p = path.join(path.dirname(process.mainModule.filename), 'data', 'cart.json');
+const sequalize = require('../utils/database');
 
-module.exports = class Cart {
-	static addProduct(id, productPrice) {
-		fs.readFile(p, (err, fileContent) => {
-			let cart = {
-				products: [],
-				totalPrice: 0
-			}
-			if (!err) {
-				cart = JSON.parse(fileContent);
-			}
-			const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
-			const existingProduct = cart.products[existingProductIndex];
-			let updatedProduct;
-			console.log(existingProduct)
-			if (existingProduct) {
-				updatedProduct = {
-					...existingProduct
-				};
-				updatedProduct.qty = updatedProduct.qty + 1;
-				cart.products = [...cart.products];
-				cart.products[existingProductIndex] = updatedProduct;
-
-			} else {
-				updatedProduct = {
-					id: id,
-					qty: 1
-				};
-				cart.products = [...cart.products, updatedProduct];
-			}
-			cart.totalPrice = cart.totalPrice + +productPrice;
-			fs.writeFile(p, JSON.stringify(cart), (err) => {
-				console.log(err);
-			});
-		});
+const Cart = sequalize.define('cart', {
+	id: {
+		type: Sequelize.INTEGER,
+		autoIncrement: true,
+		allowNull: false,
+		primaryKey: true
 	}
+});
 
-	static deleteProduct(id, prodPrice) {
-		fs.readFile(p, (err, fileContent) => {
-			if (err) {
-				return
-			}
-			const updatedCart = { ...JSON.parse(fileContent) };
-			const product = updatedCart.products.find(prod => prod.id === id);
-			if (!product) {
-				return;
-			}
-			const prodQty = product.qty;
-			updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
-			updatedCart.totalPrice = updatedCart.totalPrice - prodPrice * prodQty;
-			fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
-				console.log(err);
-			});
-		});
-	}
-	static getProducts(cb) {
-		fs.readFile(p, (err, fileContent) => {
-			const cart = JSON.parse(fileContent);
-			if (err) {
-				cb(null)
-			} else {
-				cb(cart);
-			}
-		})
-	}
-}
+module.exports = Cart;
