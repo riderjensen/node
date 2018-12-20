252190160b0853f5ac3e07cd6cac457a8f064dd4
Unknown <riderjensen@gmail.com>
2018-12-20 15:26:34 -0700

    Switching to mongo functions

--- expressMod/models/product.js
+++ expressMod/models/product.js
@@ -1,27 +1,56 @@
-const Sequelize = require('sequelize');
+const ObjectId = require('mongodb').ObjectId;
+const getDb = require('../utils/database').getDb;
 
-const sequalize = require('../utils/database');
-
-const Product = sequalize.define('product', {
-	id: {
-		type: Sequelize.INTEGER,
-		autoIncrement: true,
-		allowNull: false,
-		primaryKey: true
-	},
-	title: Sequelize.STRING,
-	price: {
-		type: Sequelize.DOUBLE,
-		allowNull: false
-	},
-	imageURL: {
-		type: Sequelize.STRING,
-		allowNull: false
-	},
-	description: {
-		type: Sequelize.STRING,
-		allowNull: false
+class Product {
+	constructor(title, price, description, imageURL, id, userId) {
+		this.title = title;
+		this.price = price;
+		this.description = description;
+		this.imageURL = imageURL;
+		this._id = id ? ObjectId(id) : null;
+		this.userId = userId;
 	}
-});
+
+	save() {
+		const db = getDb();
+		let dbOp;
+		if (this._id) {
+			dbOp = db.collection('products').updateOne({ _id: this._id }, { $set: this });
+		} else {
+			dbOp = db.collection('products').insertOne(this);
+		}
+		return dbOp
+			.then(result => {
+				console.log('Inserted');
+			})
+			.catch(err => console.log(err));
+	}
+	static fetchAll() {
+		const db = getDb();
+		return db.collection('products')
+			.find()
+			.toArray()
+			.then(products => {
+				return products
+			})
+			.catch(err => console.log(err));
+	}
+	static findById(id) {
+		const db = getDb();
+		return db.collection('products')
+			.findOne({ _id: ObjectId(id) })
+			.then(product => {
+				return product
+			})
+			.catch(err => console.log(err));
+	}
+	static deleteItem(id) {
+		const db = getDb();
+		return db.collection('products')
+			.deleteOne({ _id: ObjectId(id) })
+			.then(response => console.log('deleted'))
+			.catch(err => console.log(err));
+	}
+}
 
 module.exports = Product;
