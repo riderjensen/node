1dba1889e9b0c9df528c1404ede31efb956bd6b0
Unknown <riderjensen@gmail.com>
2018-12-20 15:27:23 -0700

    Changing DB to mongo

--- expressMod/utils/database.js
+++ expressMod/utils/database.js
@@ -1,7 +1,26 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'root', { dialect: 'mysql', host: 'localhost' })
+let _db;
 
+const mongoConnect = callback => {
+	MongoClient.connect('mongodb+srv://rider:12345678Ah@nodecourse-zfafv.mongodb.net/shop?retryWrites=true', { useNewUrlParser: true })
+		.then(client => {
+			_db = client.db();
+			callback(client);
+		})
+		.catch(err => {
+			console.log(err);
+			throw err;
+		});
+}
 
-module.exports = sequelize;
+const getDb = () => {
+	if (_db) {
+		return _db;
+	}
+	throw 'No db found!'
+}
 
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
