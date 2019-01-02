38667f6f0ab2ed908ca207d06640ec79daa1bb3f
Unknown <riderjensen@gmail.com>
2019-01-02 12:15:31 -0700

    Adding bcrypt to auth

--- expressMod/controllers/auth.js
+++ expressMod/controllers/auth.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
 
 exports.getLogin = (req, res) => {
@@ -9,15 +11,29 @@ exports.getLogin = (req, res) => {
 };
 
 exports.postLogin = (req, res) => {
-	User.findById("5c1d160bb235804634bd8b2f")
+	const { email, password } = req.body;
+	User.findOne({ email: email })
 		.then(user => {
-			req.session.isLoggedIn = true;
-			req.session.user = user;
-			req.session.save(err => {
-				if (err) console.log(err);
-				res.redirect('/');
-			})
+			if (!user) {
+				return res.redirect('/login');
+			}
+			bcrypt.compare(password, user.password)
+				.then(doMatch => {
+					if (doMatch) {
+						req.session.isLoggedIn = true;
+						req.session.user = user;
+						return req.session.save(err => {
+							if (err) console.log(err);
+							return res.redirect('/');
+						})
 
+					}
+					res.redirect('/login');
+				})
+				.catch(err => {
+					console.log(err);
+					res.redirect('/')
+				})
 		})
 		.catch(err => console.log(err))
 }
@@ -31,7 +47,25 @@ exports.getSignup = (req, res) => {
 };
 
 exports.postSignup = (req, res) => {
+	const { email, password, passwordConf } = req.body;
+	User.findOne({ email: email })
+		.then(userDoc => {
+			if (userDoc) {
+				return res.redirect('/signup');
+			}
+			return bcrypt.hash(password, 12).then(hashedPassword => {
+				const user = new User({
+					email,
+					password: hashedPassword,
+					cart: { items: [] }
+				});
+				return user.save()
+					.then(result => {
+						res.redirect('/login');
+					});
+			})
 
+		}).catch(err => console.log(err));
 }
 
 exports.postLogout = (req, res) => {
