52d2d20a187d486642747295eb8422c1968f73ae
Unknown <riderjensen@gmail.com>
2019-01-04 08:24:07 -0700

    Adding validation in routes for edit and add product

--- routes/admin.js
+++ routes/admin.js
@@ -6,20 +6,25 @@ const { body } = require('express-validator/check');
 
 const router = express.Router();
 
-router.get('/add-product', [
-	body('title').isAlphanumeric().trim().isLength({ min: 3 }),
-	body('imageURL').isURL(),
-	body('title').isFloat(),
-	body('title').trim().isLength({ min: 8 })
-], isAuth, adminController.getAddProduct);
+router.get('/add-product', isAuth, adminController.getAddProduct);
 
-router.post('/add-product', isAuth, adminController.postAddProduct);
+router.post('/add-product', [
+	body('title').isString().isLength({ min: 3 }).trim(),
+	body('imageURL').isURL(),
+	body('price').isFloat(),
+	body('description').isLength({ min: 8 }).trim()
+], isAuth, adminController.postAddProduct);
 
 router.get('/products', isAuth, adminController.getProductsAll);
 
 router.get('/edit-product/:id', isAuth, adminController.getEditProduct);
 
-router.post('/edit-product', isAuth, adminController.postEditProduct);
+router.post('/edit-product', [
+	body('title').isString().isLength({ min: 3 }).trim(),
+	body('imageURL').isURL(),
+	body('price').isFloat(),
+	body('description').isLength({ min: 8 }).trim()
+], isAuth, adminController.postEditProduct);
 
 router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
