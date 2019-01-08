a9c768ee715a7e726e8cc29a62d621f5f5daa3d2
Unknown <riderjensen@gmail.com>
2019-01-08 15:28:49 -0700

    Adding invoice function

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,7 @@
 const Product = require('../models/product');
 const Order = require('../models/order')
+const fs = require('fs');
+const path = require('path');
 
 exports.getIndex = (req, res) => {
 	Product.find()
@@ -143,4 +145,16 @@ exports.getOrders = (req, res, next) => {
 			error.httpStatusCode = 500;
 			return next(error);
 		});
+}
+
+exports.getInvoice = (req, res, next) => {
+	const orderId = req.params.orderId;
+	const invoiceName = `invoice-${orderId}.pdf`;
+	const invoicePath = path.join('data', 'invoices', invoiceName);
+	fs.readFile(invoicePath, (err, data) => {
+		if (err) {
+			return next(err);
+		}
+		res.send(data);
+	})
 }
