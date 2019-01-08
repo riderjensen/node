d71d6b0fc810db4a9be315360662776bc6655e5f
Unknown <riderjensen@gmail.com>
2019-01-08 14:54:21 -0700

    Change validation

--- routes/admin.js
+++ routes/admin.js
@@ -2,17 +2,22 @@ const express = require('express');
 
 const adminController = require('../controllers/admin');
 const isAuth = require('../middleware/is-auth');
-const { body } = require('express-validator/check');
+const {
+	body
+} = require('express-validator/check');
 
 const router = express.Router();
 
 router.get('/add-product', isAuth, adminController.getAddProduct);
 
 router.post('/add-product', [
-	body('title').isString().isLength({ min: 3 }).trim(),
-	body('imageURL').isURL(),
+	body('title').isString().isLength({
+		min: 3
+	}).trim(),
 	body('price').isFloat(),
-	body('description').isLength({ min: 8 }).trim()
+	body('description').isLength({
+		min: 8
+	}).trim()
 ], isAuth, adminController.postAddProduct);
 
 router.get('/products', isAuth, adminController.getProductsAll);
@@ -20,10 +25,13 @@ router.get('/products', isAuth, adminController.getProductsAll);
 router.get('/edit-product/:id', isAuth, adminController.getEditProduct);
 
 router.post('/edit-product', [
-	body('title').isString().isLength({ min: 3 }).trim(),
-	body('imageURL').isURL(),
+	body('title').isString().isLength({
+		min: 3
+	}).trim(),
 	body('price').isFloat(),
-	body('description').isLength({ min: 8 }).trim()
+	body('description').isLength({
+		min: 8
+	}).trim()
 ], isAuth, adminController.postEditProduct);
 
 router.post('/delete-product', isAuth, adminController.postDeleteProduct);
