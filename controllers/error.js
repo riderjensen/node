1ebdc9034dfe7600cd66e2dc47365db4bf439012
Unknown <riderjensen@gmail.com>
2019-01-08 14:54:32 -0700

    adding authentication back

--- controllers/error.js
+++ controllers/error.js
@@ -2,6 +2,8 @@ exports.get404 = (req, res) => {
 	res.status(404).render('404', {
 		title: "Page not found",
 		path: "/404",
+		isAuthenticated: req.session.isLoggedIn
+
 	});
 }
 
@@ -9,5 +11,7 @@ exports.get500 = (req, res) => {
 	res.status(500).render('500', {
 		title: "Error",
 		path: "/500",
+		isAuthenticated: req.session.isLoggedIn
+
 	});
 }
