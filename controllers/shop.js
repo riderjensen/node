1c5c08c0ae4ad4216414b85983dbdd1fa1e1b4fa
Unknown <riderjensen@gmail.com>
2019-01-09 16:55:31 -0700

    Put pagination on controller for products

--- controllers/shop.js
+++ controllers/shop.js
@@ -32,19 +32,28 @@ exports.getIndex = (req, res) => {
 }
 
 exports.getProducts = (req, res, next) => {
-	Product.find()
+	const page = +req.query.page || 1;
+	let totalItems;
+	Product.find().countDocuments().then(numProds => {
+			totalItems = numProds;
+			return Product.find()
+				.skip((page - 1) * ITEMS_PER_PAGE)
+				.limit(ITEMS_PER_PAGE)
+		})
 		.then(products => {
 			res.render('shop/product-list', {
 				products: products,
 				title: 'Products',
-				path: '/product-list',
+				path: '/products',
+				currentPage: page,
+				hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+				hasPreviousPage: page > 1,
+				nextPage: page + 1,
+				previousPage: page - 1,
+				lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
 			});
 		})
-		.catch(err => {
-			const error = new Error(err);
-			error.httpStatusCode = 500;
-			return next(error);
-		});
+		.catch(err => next(err));
 }
 
 exports.getSingleProduct = (req, res, next) => {
